872c9780bfa6c46d5a70337c00af5b4f36226f95
皓东haocity <ureygt@gmail.com>
2017-04-22 00:10:23 +0800

    再次修复..缓存段显示

--- tdplayer.js
+++ tdplayer.js
@@ -376,19 +376,36 @@ function Tdplayer(Element, src, poster) {
         $d("tranger-a").style.width = videotime / tdplayer.alltime * 100 + "%";
         var buff = tdplayer.videoelearr[tdplayer.nowduan].buffered;
         //判断缓存段
-        if(buff)
-        {
-        	var t=buff.end(0);
-        	var oldduan = tdplayer.nowduan - 1, oldtime = 0, time2 = 0;
-        	for (var i = 0; i <= oldduan; i++) {
-                oldtime += tdplayer.videotimearr[i];
+//      if(buff)
+//      {
+//      	var t=buff.end(0);
+//      	var oldduan = tdplayer.nowduan - 1, oldtime = 0, time2 = 0;
+//      	for (var i = 0; i <= oldduan; i++) {
+//              oldtime += tdplayer.videotimearr[i];
+//          }
+//      	time2 = oldtime + t;
+//      	var width = time2 / tdplayer.alltime * 100 + "%";
+//          if ($d("tranger-c").style.width != width) {
+//              $d("tranger-c").style.width = width;
+//          }
+//      	
+//      }
+        
+        for (var i = 0; i < buff.length; i++) {
+            if (buff.start(i) <= smalltime && smalltime < buff.end(i)) {
+                var oldduan = tdplayer.nowduan - 1, oldtime = 0, time2 = 0;
+                for (var i = 0; i <= oldduan; i++) {
+                    oldtime += tdplayer.videotimearr[i];
+                }
+				if (tdplayer.videoelearr[tdplayer.nowduan].buffered.end(i)) {
+                    time2 = oldtime + tdplayer.videoelearr[tdplayer.nowduan].buffered.end(i);
+                }
+                var width = time2 / tdplayer.alltime * 100 + "%";
+                if ($d("tranger-c").style.width != width) {
+                    $d("tranger-c").style.width = width;
+                }
+                
             }
-        	time2 = oldtime + t;
-        	var width = time2 / tdplayer.alltime * 100 + "%";
-            if ($d("tranger-c").style.width != width) {
-                $d("tranger-c").style.width = width;
-            }
-        	
         }
         if (tdplayer.nowdata) {
             var inttime = parseInt(videotime * 10);
