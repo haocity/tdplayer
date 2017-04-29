fef87d2b77e5ffd64196f5d3f7cc064b7a3cd645
皓东haocity <ureygt@gmail.com>
2017-04-29 19:30:03 +0800

    上次错脚本了  刚刚传的是本地调试用的

--- script.js
+++ script.js
@@ -4,12 +4,12 @@
     var script = document.createElement('script');
     script.setAttribute('type', 'text/javascript');
     script.setAttribute('charset', 'UTF-8');
-    script.setAttribute('src', 'http://127.0.0.1:8020/tdplayer/tdplayer.js');
+    script.setAttribute('src', 'https://app.haotown.cn/td/tdplayer.min.js');
     document.head.appendChild(script);
      var link = document.createElement('link');
     link.setAttribute('type', 'text/css');
     link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('href', 'http://127.0.0.1:8020/tdplayer/style.css');
+    link.setAttribute('href', 'https://app.haotown.cn/td/tdplayer.css');
     document.head.appendChild(link);
     loadtd();
 function loadtd(){
