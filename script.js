357552636a2bf1b7c9dbe88e45936bda22d1d62f
皓东haocity <ureygt@gmail.com>
2017-04-30 00:06:35 +0800

    支持多p视频啦！

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 (function () {
-	var warp = document.getElementById('player');
-	warp.innerHTML=null;
-    var script = document.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.setAttribute('charset', 'UTF-8');
-    script.setAttribute('src', 'https://app.haotown.cn/td/tdplayer.min.js');
-    document.head.appendChild(script);
-     var link = document.createElement('link');
-    link.setAttribute('type', 'text/css');
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('href', 'https://app.haotown.cn/td/style.css');
-    document.head.appendChild(link);
-    loadtd();
-function loadtd(){
-	try{
-		acplay(warp,pageInfo.id);
+	var thisurl=window.location.href;
+	if(thisurl.indexOf("acfun.cn") < 0||thisurl.indexOf("acfun.tv") < 0||thisurl.indexOf("aixifan.com") < 0){
+        tmp = thisurl.split("/");
+        thisac = tmp[tmp.length - 1];
+        console.log( thisac);
+		var warp = document.getElementById('player');
+		if(warp){
+			warp.innerHTML=null;
+		    var script = document.createElement('script');
+		    script.setAttribute('type', 'text/javascript');
+		    script.setAttribute('charset', 'UTF-8');
+		    //script.setAttribute('src', 'http://127.0.0.1:8020/tdplayer/tdplayer.js');
+		    script.setAttribute('src', 'https://app.haotown.cn/td/tdplayer.min.js');
+		    document.head.appendChild(script);
+		    var link = document.createElement('link');
+		    link.setAttribute('type', 'text/css');
+		    link.setAttribute('rel', 'stylesheet');
+		    //link.setAttribute('href', 'http://127.0.0.1:8020/tdplayer/style.css');
+		    link.setAttribute('href', 'https://app.haotown.cn/td/style.css');
+		    document.head.appendChild(link);
+		    loadtd(thisac);
+	   }else{
+	   	alert('暂时不支持的视频');
+	   }
+	}else{
+		alert('请进入acfun再说。。');
 	}
-	catch(e){
-		console.log('loading..');
-		setTimeout(loadtd,100)
+	
+function loadtd(ac){
+	var t=ac;
+	if(t.indexOf("ac")==0){
+		var id=ac.slice(2);
+		console.log(id);
+		try{
+			acplay(warp,id);
+		}catch(e){
+			console.log(e);
+			setTimeout(loadtd,100,ac)
+		}
+		
+	}else{
+		alert('暂时不支持的视频');
 	}
 }
 }());
