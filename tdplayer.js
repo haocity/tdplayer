a071075b6854535987336e1a560b43ccf7d292ef
皓东haocity <ureygt@gmail.com>
2017-05-04 19:02:42 +0800

    修复全屏后播放速度仍按全屏速度的bug

--- tdplayer.js
+++ tdplayer.js
@@ -769,6 +769,37 @@ function tdstart(Element,src,data,poster,videotype) {
             };
         }
     }
+    function tpeixtfull(){
+    	setTimeout(function(){
+    	dmspeend(tp.width / 100);
+    	tp.width = tp.ele.video.offsetWidth;
+        var e = tp.ele.danmu_warp.getElementsByTagName("div");
+        dmspeend(tp.width / 100);
+            for (var i = e.length - 1; i >= 0; i--) {
+	            if (hasClass(e[i], "tp-left")) {
+	                 e[i].style.transform = "translateX(-" + tp.width + "px)";
+	            }
+            }
+    	},1000);
+    }
+    document.addEventListener('webkitfullscreenchange',function(c){
+    	if(document.webkitFullscreenElement){
+    		console.log('进入全屏');
+    		}else{
+    		console.log('退出全屏');
+			tpeixtfull();
+    	}
+    	
+    });
+    document.addEventListener('mozfullscreenchange',function(c){
+    	if(document.mozFullscreenElement){
+    		console.log('进入全屏')
+    		}else{
+    		console.log('退出全屏');
+    		tpeixtfull();
+    	}
+    	
+    });
     tdplayer.ele.danmu_warp.onmousedown = function(event) {
         var container = tdplayer.ele.tdplayer;
         var rightmenu = tdplayer.ele.tp_rightmenu;
