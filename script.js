1f542b5b0a68a4aba7ddc59490d223ef8fd56ce9
皓东haocity <ureygt@gmail.com>
2017-05-10 23:19:30 +0800

    修正下

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
-(function () {
-	var thisurl=window.location.href;
-	if(thisurl.indexOf("acfun.cn") < 0||thisurl.indexOf("acfun.tv") < 0||thisurl.indexOf("aixifan.com") < 0){
-        tmp = thisurl.split("/");
-        thisac = tmp[tmp.length - 1];
-        console.log( thisac);
-		var warp = document.getElementById('player');
-		if(warp){
-			warp.innerHTML=null;
-		    var script = document.createElement('script');
-		    script.setAttribute('type', 'text/javascript');
-		    script.setAttribute('charset', 'UTF-8');
-		    //script.setAttribute('src', 'http://127.0.0.1:8020/tdplayer/tdplayer.js');
-		    script.setAttribute('src', 'https://app.haotown.cn/td/tdplayer.min.js');
-		    document.head.appendChild(script);
-		    var link = document.createElement('link');
-		    link.setAttribute('type', 'text/css');
-		    link.setAttribute('rel', 'stylesheet');
-		    //link.setAttribute('href', 'http://127.0.0.1:8020/tdplayer/style.css');
-		    link.setAttribute('href', 'https://app.haotown.cn/td/style.css');
-		    document.head.appendChild(link);
-		    script.onload=function(){
-		    	loadtd(thisac);
-		    }
-	   }else{
-	   	alert('暂时不支持的视频');
-	   }
-	}else{
-		alert('请进入acfun再说。。');
-	}
-	
-function loadtd(ac){
-	var t=ac;
-	if(t.indexOf("ac")==0){
-		var id=ac.slice(2);
-		console.log(id);
-			Tdplayer(warp,id);
-	}else{
-		alert('暂时不支持的视频');
-	}
-}
-}());
+(function() {
+    var thisurl = window.location.href;
+    if(thisurl.indexOf("acfun.cn") < 0||thisurl.indexOf("acfun.tv") < 0||thisurl.indexOf("aixifan.com") < 0){
+	    var warp = document.querySelector(".player>#player");
+	    if (warp) {
+	        tmp = thisurl.split("/");
+	        thisac = tmp[tmp.length - 1];
+	        warp.innerHTML = null;
+	        var script = document.createElement("script");
+	        script.setAttribute("type", "text/javascript");
+	        script.setAttribute("charset", "UTF-8");
+	        script.setAttribute('src', 'https://app.haotown.cn/td/tdplayer.min.js');
+	        //script.setAttribute('src', 'http://127.0.0.1:8020/tdplayer/tdplayer.js');
+	        document.head.appendChild(script);
+	        var link = document.createElement("link");
+	        link.setAttribute("type", "text/css");
+	        link.setAttribute("rel", "stylesheet");
+	        link.setAttribute('href', 'https://app.haotown.cn/td/style.css');
+	        //link.setAttribute('href', 'http://127.0.0.1:8020/tdplayer/style.css');
+	        document.head.appendChild(link);
+	        script.onload = function() {
+	            if (thisac.indexOf("ac") == 0) {
+	                var id = thisac.slice(2);
+	                Tdplayer(warp, id);
+	            } else {
+	                $.info("不支持。 /(ㄒoㄒ)/~~");
+	            }
+	        };
+	    }
+    }else{
+    	alert('请进入acfun再说。。');
+    }
+})();
