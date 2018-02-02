986be5cea41ab277a736e30e66f55ccab4ba3126
疯狂减肥带 <ureygt@gmail.com>
2018-02-02 15:36:32 +0800

    没有亡！只是技术性调整

--- src/index.js
+++ src/index.js
@@ -1664,13 +1664,13 @@ class Tplayer {
 	showbar() {
 		if(!this.phone) {
 			if(this.bar) {
-				clearTimeout(_this.bar)
+				clearTimeout(this.bar)
 			}
 			if(this.ele.video_con.style.opacity != '1') {
 				this.ele.video_con.style.opacity = "1";
 			}
 			let _this = this;
-			setTimeout(function() {
+			this.bar=setTimeout(function() {
 				_this.ele.video_con.style.opacity = "0";
 			}, 3000);
 		}
